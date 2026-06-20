0a13e532e89f3ec81614af872dedc5287c8681a7
TejaThomas64 <tejathomas26@gmail.com>
2026-06-20 10:56:12 +0530

    Host fix

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
+
 
 const cron = require("node-cron")
 
